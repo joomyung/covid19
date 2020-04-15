be8ed0d19809eca7c3a60605faeefb227550abad
Matt Song <songjoomyung@gmail.com>
2020-04-14 22:18:05 -0700

    remove an error data and upload dataset

--- database/transforms.py
+++ database/transforms.py
@@ -8,17 +8,18 @@ from dash.dependencies import Input, Output
 import pandas as pd
 
 # read data
-# url_con = 'csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
-# url_rec = 'csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
-# url_fat = 'csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
-
-url_con = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-url_rec = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
-url_fat = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+url_con = 'database/time_series_covid19_confirmed_global.csv'
+url_rec = 'database/time_series_covid19_recovered_global.csv'
+url_fat = 'database/time_series_covid19_deaths_global.csv'
 
 df_con = pd.read_csv(url_con)
 df_rec = pd.read_csv(url_rec)
 df_fat = pd.read_csv(url_fat)
+
+# drop an error data
+df_con = df_con.drop(index=231)
+df_rec = df_rec.drop(index=231)
+df_fat = df_fat.drop(index=231)
 
 df_act = df_con.copy() # active = confirmed - recovered - fatal
 df_act.iloc[:, 4:] = df_act.iloc[:, 4:] - df_rec.iloc[:, 4:] - df_fat.iloc[:, 4:]
